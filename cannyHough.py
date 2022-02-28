fd2bdd7c3f54d57a268091dd657e368053149478
Luke M <lmalek423@gmail.com>
2022-02-28 14:24:34 -0600

    lil demo program

--- cannyHough.py
+++ cannyHough.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import cv2
 
-
+#Program won't work unless you have an input image titled as the following:
 img = cv2.imread('numLine.jpg',0 )
 img = cv2.resize(img, (0,0), fx = 1, fy = 1)
 current = img.copy()
@@ -14,14 +14,13 @@ pressed = False
 def click(event, x, y, flags, param):
 	global img, pressed
 	update = False
+	#Read in x and y mouse coords and take not eof which one is bigger. we scale down a bit for finer tuning
 	smol = min(x,y)
-	
 	big = max(x,y)
-
-
-
 	smol = smol/2
 	big = big/2
+
+
 	if event == cv2.EVENT_LBUTTONDOWN:
 		current = cv2.Canny(img,smol,big)
 		pressed = True
@@ -37,6 +36,10 @@ def click(event, x, y, flags, param):
 
 	if update:
 		cdstP = cv2.cvtColor(current, cv2.COLOR_GRAY2BGR)
+		
+		#Run a probabilistic hough transform on the image. last 2 params are of utmost importance
+		#500 indicates that we are looking for lines 500 pixels long
+		# 40 indicates that we will allow a line to be considered so long as no gaps of onger than 40 pixels exist within it.
 		linesP = cv2.HoughLinesP(current, 1, np.pi / 180, 50, None, 500, 40)
 
 		if linesP is not None:
