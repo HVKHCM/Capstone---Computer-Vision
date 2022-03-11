f0038da02bba0d4280599b501738e32f5a7d03fa
Luke M <lmalek423@gmail.com>
2022-03-10 20:41:23 -0600

    reorients cropped lines now

--- isolateLines.py
+++ isolateLines.py
@@ -1,6 +1,26 @@
 import numpy as np
 import cv2
 
+def cropToLine(img, l, bufferSize=0):
+    length = np.sqrt((l[0]-l[2])**2 + (l[1]-l[3])**2)
+    theta = np.arctan((l[1]-l[3])/(l[0]-l[2]))
+    print(length, theta)
+    (h,w) = img.shape[:2]
+
+    lineCenter = ((l[0]+l[2])//2, (l[1]+l[3])//2)
+    M = cv2.getRotationMatrix2D(lineCenter, theta/np.pi * 180, 1.0)
+    rotated = cv2.warpAffine(img, M, (w,h))
+
+    x1 = int(max(0, lineCenter[0]- length//2-bufferSize))
+    x2 = int(min(w, lineCenter[0]+ length//2+bufferSize))
+
+    y1 = int(max(0,lineCenter[1]-10- bufferSize))
+    y2 = int(min(h,lineCenter[1]+10 + bufferSize))
+
+
+    return rotated[y1:y2,  x1:x2 ]
+
+
 minv = 15
 maxv = 105
 
@@ -16,15 +36,8 @@ cdstP = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 
 for i in range(0, len(linesP)):
     l = linesP[i][0]
-    starty = min(l[0],l[2])
-    stopy = max(l[0],l[2])
-
-    startx = min(l[1],l[3])
-    stopx = max(l[1],l[3])
-
-    crop = img[startx:stopx, starty:stopy]
-    cv2.imshow("Crop numba " + str(i), crop)
     cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+    cv2.imshow("reoriented" + str(i), cropToLine(img, l,10))
 
 cv2.imshow("original", cdstP)
 
