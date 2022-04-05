b101efefe9982c39ba1dca1c0608debffb9321b1
Luke M <lmalek423@gmail.com>
2022-04-05 12:51:15 -0500

    slight adjustments and updates

--- rulerRegion.py
+++ rulerRegion.py
@@ -1,12 +1,59 @@
 import cv2
 import numpy as np
+from scipy.signal import find_peaks
+from skimage.draw import line
+
+def discretize(l):
+    x1, y1, x2, y2 = l
+    return np.asarray(list(zip(*line(*(x1, y1 ), *(x2 , y2 )))))
+
+
+
+
+def optimize_line_alignment(img_gray, line_end_points):
+    # Shift endpoints to find optimal alignment with black line in the origial image
+    opt_line_mean = 255
+    x1, y1, x2, y2 = line_end_points
+    for dx1 in range(-3, 4):
+        for dy1 in range(-3, 4):
+            for dx2 in range(-3, 4):
+                for dy2 in range(-3, 4):
+                    line_discrete = np.asarray(
+                        list(zip(*line(*(x1 + dx1, y1 + dy1), *(x2 + dx2, y2 + dy2))))
+                    )
+                    line_pixel_values = img_gray[
+                        line_discrete[:, 1], line_discrete[:, 0]
+                    ]
+                    line_mean = np.mean(line_pixel_values)
+                    if line_mean < opt_line_mean:
+                        opt_line_end_points = np.array(
+                            [x1 + dx1, y1 + dy1, x2 + dx2, y2 + dy2]
+                        )
+                        opt_line_discrete = line_discrete
+                        opt_line_mean = line_mean
+    return opt_line_end_points, opt_line_discrete
 
 def lineLen(line):
     return np.sqrt((line[0]-line[2])**2 + (line[1]-line[3])**2)
 
+def midPoint(l):
+    return [(l[0]+l[2])/2, (l[1]+l[3])/2]
+
 def mb(l):
     return ((l[1]-l[3])/(l[0]-l[2]), -l[0]* (l[1]-l[3])/(l[0]-l[2]) + l[1]) 
 
+def ab(l):
+    #Gives standard form a,b for a given line 1. assume c = 1
+    x1,y1,x2,y2 = l
+    return (-(y1-y2)/(x2*y1-x1*y2), -(x2-x1)/(x2*y1-x1*y2))
+
+def pointDist(p, l):
+    #gives normal distance between a point p and line l.
+    a, b = ab(l)
+    x1, y1 = p
+    
+    return abs(a*x1 + b*y1 + 1)/np.sqrt(a**2+b**2)
+    
 def distApart(a,b):
     #only to be used on basically parallel lines
     b1 = mb(b)[1]
@@ -19,12 +66,13 @@ def parallelism(a,b):
     prod = abs(np.dot(va,vb))
     return prod/lineLen(a)/lineLen(b) 
 
-a = [0,0,10,1]
+a = [0,2,10,2]
 b = [20,18,30,18]
+print(discretize(a))
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('numLineData/IMG_1708.MOV')
+cap = cv2.VideoCapture('numLineData/IMG_1709.MOV')
 # Check if camera opened successfully
 if (cap.isOpened()):
     ret, img = cap.read()
@@ -38,19 +86,21 @@ def process(img):
     width = img.shape[1]
     maxVal = 1000
     minVal = 700
+    
+    candidates = 10
     while True:
         edges = cv2.Canny(img, minVal, maxVal)
         edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, (5, 5), iterations=1)
         lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, width//3, width//30)
-        if lines is not None and len(lines)>15 :
+        if lines is not None and len(lines)>candidates :
             break
-        minVal *= 0.97
-        maxVal *= 0.97
+        minVal *= 0.9
+        maxVal *= 0.9
         maxVal = int(maxVal)
         minVal = int(minVal)
     
     #So we found a good few lines, concatenate to less than 10
-    lines = lines[:15]
+    lines = lines[:candidates]
     lines = lines.squeeze()
     
     #init matrix to score line-bounding likelihood
@@ -59,8 +109,9 @@ def process(img):
         for j, line2 in enumerate(lines[i+1:]):
             init = parallelism(line1, line2)
             if init > 0.999:
-                d = distApart(line1, line2)
-                init += min(d/20, 1, 6 - d/50)
+                #d = distApart(line1, line2)
+                d = pointDist(midPoint(line1), line2)
+                init += min(d/40, 1, 6 - d/50)
             #Likeliness value first checks if two lines are basically parallel, then if they are, 
             #additional points are added if they are a fair distance away, but not too far.
             likely[i][j] = init
@@ -68,6 +119,10 @@ def process(img):
     
     #extract the VERY best line
     i, j = np.unravel_index(likely.argmax(), likely.shape)
+    veryBest = []
+    veryBest.append(lines[i])
+    veryBest.append(lines[i+j+1])
+    
     
     #Consider making this a threshold thing. Like if likelihood over 1.5 mark them down.
     best = []
@@ -75,15 +130,25 @@ def process(img):
         for j in range(0, len(likely)):
             if likely[i][j] >= 1.9:
                 best.append(lines[i])
-                best.append(lines[i+j+1])
+                best.append(lines[i+j+1])       
     
     #display all lines, then best ones in green  
     display = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
     for l in lines:
         cv2.line(display, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
         
-    for i, l in enumerate(best):
-        cv2.line(display, (l[0], l[1]), (l[2], l[3]), ((i//2)* 510//len(best),255 - (i//2)* 510//len(best) ,0), 3, cv2.LINE_AA)
+    #print(best)
+    
+    for l in best:
+        cv2.line(display, (l[0], l[1]), (l[2], l[3]), (255,0,0), 3, cv2.LINE_AA)
+    
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    
+    for l in veryBest:
+        cv2.line(display, (l[0], l[1]), (l[2], l[3]), (0,255,0), 3, cv2.LINE_AA)
+        l, newlinedisc = optimize_line_alignment(img_gray, l)
+        cv2.line(display, (l[0], l[1]), (l[2], l[3]), (255,0,255), 3, cv2.LINE_AA)
     
     return display
 
@@ -111,6 +176,8 @@ while True:
         display = img
     elif ch & 0xFF == ord('p'):
         display = process(img)
+    elif ch & 0xFF == ord('c'):
+        display = cv2.Canny(img, 50,100)
 
     cv2.imshow("window", display)
 
@@ -126,3 +193,8 @@ for i in range(0,12):
     cv2.destroyAllWindows()
     
     
+img = cv2.imread('numLine.jpeg')
+cv2.imshow("window", process(img))
+cv2.waitKey()
+for i in range(0,12):
+    cv2.destroyAllWindows()
